960d60bfc8454f619bfdf0bbe8faa480efbb37a9
TeonaTanasa <teona.tanasa@principal33.com>
2021-08-03 09:24:05 +0300

    using git in vs

--- HelloWorld.Console/Program.cs
+++ HelloWorld.Console/Program.cs
@@ -6,7 +6,7 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Cluj!");
+            Console.WriteLine("Hello Cluj!!!");
         }
     }
 }
