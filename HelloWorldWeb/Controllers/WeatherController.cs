77982ad2de625911e63dbee2fbadc24f9b558f02
TeonaTanasa <teona.tanasa@principal33.com>
2021-08-12 11:26:53 +0300

    added WeatherController, DailyWeatherRecord and WeatherType

---
+++ HelloWorldWeb/Controllers/WeatherController.cs
@@ -0,0 +1,65 @@
+﻿using HelloWorldWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace HelloWorldWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly string longitude = "46.7700";
+        private readonly string latitude = "23.5800";
+        private readonly string apiKey = "c969b66bb3c8e3fe32d4485a1623f42c";
+
+        // GET: api/<WeatherController>
+        [HttpGet]
+        public IEnumerable<DailyWeatherRecord> Get()
+        {
+            var client = new RestClient($"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=hourly,minutely&appid={apiKey}");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            return ConvertResponseToWeatherForecastList(response.Content);
+        }
+
+        public IEnumerable<DailyWeatherRecord> ConvertResponseToWeatherForecastList(string content)
+        {
+            return new DailyWeatherRecord[]
+            {
+                new DailyWeatherRecord(new DateTime(2021, 8, 12), 22.0f, WeatherType.Mild),
+                new DailyWeatherRecord(new DateTime(2021, 8, 13), 25.0f, WeatherType.Mild)
+            };
+        }
+
+        // GET api/<WeatherController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<WeatherController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<WeatherController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<WeatherController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
