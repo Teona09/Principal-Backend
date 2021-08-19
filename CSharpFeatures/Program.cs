670372d8b5d399dbf09e83c8e7a7d431321a4ec9
TeonaTanasa <teona.tanasa@principal33.com>
2021-08-19 10:26:04 +0300

    created CSharpFeature project

---
+++ CSharpFeatures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace CSharpFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeamMember teamMember = new TeamMember() { Name = "Member1" };
+            string jsonString = JsonSerializer.Serialize(teamMember);
+            Console.WriteLine(jsonString);
+            File.WriteAllText("TeamMember.json", jsonString);
+            var readText = File.ReadAllTextAsync("TeamMember.json");
+            readText.Wait();
+            var expectedOutput = readText.Result;
+            TeamMember teamMemberDeserialized = JsonSerializer.Deserialize<TeamMember>(expectedOutput);
+            Console.WriteLine(teamMemberDeserialized);
+        }
+    }
+}
---
+++ CSharpFeatures/TeamMember.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpFeatures
+{
+    using System;
+    using System.Diagnostics;
+
+    [DebuggerDisplay("{Name}[{Id}]")]
+    public class TeamMember
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+    }
+}
