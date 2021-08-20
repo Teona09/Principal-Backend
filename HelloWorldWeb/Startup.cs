6545f52a1bda876d18e83a2917ba9cc9348405da
TeonaTanasa <teona.tanasa@principal33.com>
2021-08-20 10:55:20 +0300

    added MessageHub. connected MessageHub to Startup.

--- HelloWorldWeb/Startup.cs
+++ HelloWorldWeb/Startup.cs
@@ -42,6 +42,7 @@ namespace HelloWorldWeb
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
             });
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,6 +75,8 @@ namespace HelloWorldWeb
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                //endpoints.MapRazorPages();
+                endpoints.MapHub<MessageHub>("/messagehub");
             });
         }
     }
