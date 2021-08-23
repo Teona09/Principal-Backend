b51bcd7a402b991a73f934d5b0a387a93e233188
TeonaTanasa <teona.tanasa@principal33.com>
2021-08-23 10:44:20 +0300

    created IBroadcastService and BroadcastService.

---
+++ HelloWorldWeb/Services/BroadcastService.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorldWeb.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using HelloWorldWeb.Models;
+    using Microsoft.AspNetCore.SignalR;
+
+    public class BroadcastService : IBroadcastService
+    {
+        private readonly IHubContext<MessageHub> messageHub;
+
+        public BroadcastService(IHubContext<MessageHub> messageHub)
+        {
+            this.messageHub = messageHub;
+        }
+
+        public void NewTeamMemberAdded(TeamMember member, int id)
+        {
+            messageHub.Clients.All.SendAsync("NewTeamMemberAdded", member, member.Id);
+        }
+    }
+}
---
+++ HelloWorldWeb/Services/IBroadcastService.cs
@@ -0,0 +1,9 @@
+﻿using HelloWorldWeb.Models;
+
+namespace HelloWorldWeb.Services
+{
+    public interface IBroadcastService
+    {
+        void NewTeamMemberAdded(TeamMember member, int id);
+    }
+}
