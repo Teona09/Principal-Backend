31492f62c6796bb21b004f9d23fee596f154a6c7
TeonaTanasa <teona.tanasa@principal33.com>
2021-08-26 11:23:57 +0300

    Added Roles in Startup.

--- HelloWorldWeb/Startup.cs
+++ HelloWorldWeb/Startup.cs
@@ -53,8 +53,9 @@ namespace HelloWorldWeb
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+               .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddSingleton<IWeatherControllerSettings, WeatherControllerSettings>();
             services.AddScoped<ITeamService, DbTeamService>();
