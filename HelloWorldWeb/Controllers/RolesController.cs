5ad14bbe9eefd460c31fe2f6440d24626465787a
TeonaTanasa <teona.tanasa@principal33.com>
2021-08-26 11:22:28 +0300

    Added RoleController.

---
+++ HelloWorldWeb/Controllers/RolesController.cs
@@ -0,0 +1,44 @@
+﻿namespace HelloWorldWeb.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    public class RolesController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        // GET: RolesController
+        public ActionResult Index()
+        {
+            return View(roleManager.Roles);
+        }
+
+        // GET: RolesController/Create
+        public ActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+
+        // POST: RolesController/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(IdentityRole role)
+        {
+            try
+            {
+                await roleManager.CreateAsync(role);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
