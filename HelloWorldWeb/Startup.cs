46b8fee08f77a1b80785e22d17d953fb851e77cc
TeonaTanasa <teona.tanasa@principal33.com>
2021-08-27 17:52:49 +0300

    change role from Administrators to Administrator. made sure is always an Administrator in the app.

--- HelloWorldWeb/Startup.cs
+++ HelloWorldWeb/Startup.cs
@@ -116,7 +116,7 @@ namespace HelloWorldWeb
         {
             var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
             var user = await userManager.FindByNameAsync("teona@principal.com");
-            await userManager.AddToRoleAsync(user, "Administrators");
+            await userManager.AddToRoleAsync(user, "Administrator");
         }
     }
 }
